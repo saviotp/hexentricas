60f188a12bcef140e3655cdcba0a32078d47ed64
Jonatan Santos <jonatan.vidal@alu.ufc.br>
2025-02-23 19:37:41 -0300

    style: torna o site responsivo
    - Reorganiza e deixa as seções flex para melhor responsividade
    - Adiciona o menu de hambúrguer para a barra de navegação em telas menores
    - Transforma a parte dos contatos em uma lista

--- js/index.js
+++ js/index.js
@@ -2,3 +2,8 @@ function scrollToSection(id) {
   let section = document.getElementById(id);
   section.scrollIntoView();
 }
+
+function toggleMenu() {
+  let navbar = document.getElementById("navbar");
+  navbar.classList.toggle("active");
+}
